144070ebe737538b055d1388eae95163da500352
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2016-08-15 22:40:47 +0300

    Icons changed from Google to FontAwesome.

--- wallpaper_creators/js/bin/pali-wallpaper-maker1.js
+++ wallpaper_creators/js/bin/pali-wallpaper-maker1.js
@@ -9,6 +9,7 @@ function randRGBA(min,max)
 
 /* Getting the text from the form */
     headingName = document.getElementById("headingName").value ;
+    greetingType = document.getElementById("greetingType").value ;
     textSize = document.getElementById("textSize").value ;
     fromName = document.getElementById("fromName").value ;
     toName = document.getElementById("toName").value ;
@@ -24,9 +25,9 @@ function randRGBA(min,max)
     rulerBottom = "<br><br><img src='images/" + ornamentStyle + "ornbottom.png' width='90%'>";
 
 /********* Printing the formmated text to an ID in the HTML page ******/
-document.getElementById("randcolorRGBA").innerHTML = "<div class='pali' style=\"font-size:" + textSize + "px ;background-color:" + colorRGBA + ";\"><i class=\"fa fa-heart fa-2x\"></i><p class='light'>" + headingName + "</p>" + rulerTop + quoteText + rulerBottom + "<p class='light'>" + footerText + "</p>" + logoImageUrl + "</div>";
+document.getElementById("randcolorRGBA").innerHTML = "<div class='pali' style=\"font-size:" + textSize + "px ;background-color:" + colorRGBA + ";\">" + greetingType + "<p class='light'>" + headingName + "</p>" + rulerTop + quoteText + rulerBottom + "<p class='light'>" + footerText + "</p>" + logoImageUrl + "</div>";
 
-document.getElementById("randcolorRGBAtext").innerHTML = "<div class='pali' style=\"font-size:" + textSize + "px ;color:" + colorRGBA + ";\"><i class=\"fa fa-heart fa-2x\"></i><p class='dark'>" + headingName + "</p>" + rulerTop + quoteText + rulerBottom + "<p class='dark'>" + footerText + "</p>" + logoImageUrl + "</div>";
+document.getElementById("randcolorRGBAtext").innerHTML = "<div class='pali' style=\"font-size:" + textSize + "px ;color:" + colorRGBA + ";\">" + greetingType + "<p class='dark'>" + headingName + "</p>" + rulerTop + quoteText + rulerBottom + "<p class='dark'>" + footerText + "</p>" + logoImageUrl + "</div>";
 
 
 }

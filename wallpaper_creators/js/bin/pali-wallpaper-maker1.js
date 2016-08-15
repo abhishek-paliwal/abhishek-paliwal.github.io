19e4e5403d705cc7c0da594f4f7811266b051c9e
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2016-08-15 23:28:45 +0300

    Variable name corrected.

--- wallpaper_creators/js/bin/pali-wallpaper-maker1.js
+++ wallpaper_creators/js/bin/pali-wallpaper-maker1.js
@@ -13,7 +13,7 @@ function randRGBA(min,max)
     textSize = document.getElementById("textSize").value ;
     fromName = document.getElementById("fromName").value ;
     toName = document.getElementById("toName").value ;
-          toname = " for " + toName;
+          toName = " for " + toName;
     quoteText = document.getElementById("quoteText").value ;
     logoImageUrl = document.getElementById("logoImageUrl").value ;
           logoImageUrl = "<img src='" + logoImageUrl + "' height='90px'>" ;

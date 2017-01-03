5eda23757c36018a510da5b89a1b33b73d9dd0d0
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2017-01-03 03:26:02 +0200

    Variable calculations for Logo Image URL added.

--- wallpaper_creators/js/pali-qlocktype.js
+++ wallpaper_creators/js/pali-qlocktype.js
@@ -11,6 +11,43 @@ function randRGBA(min,max)
         /* Getting the text from the form */
 
         headingName = document.getElementById("headingName").value.toUpperCase(); ;
+
+        logoImageUrl = document.getElementById("logoImageUrl").value ;
+                if (logoImageUrl == "ap1") {
+                    logoImageUrl = "http://downloads.concepro.com/dropbox-public-files/logos/1-logos-abhishek/Abhishek-Paliwal-125-Blacknwhite-Circle-footername.png";
+                } else if (logoImageUrl == "ap2") {
+                  logoImageUrl = "http://downloads.concepro.com/dropbox-public-files/logos/1-logos-abhishek/Abhishek-Paliwal-125-Circle-fullname.png";
+                } else if (logoImageUrl == "ap3") {
+                  logoImageUrl  = "http://downloads.concepro.com/dropbox-public-files/logos/1-logos-abhishek/Album-Cover-Abhishek-Paliwal-Lorez.jpg";
+                } else if (logoImageUrl == "ap4") {
+                  logoImageUrl  = "http://downloads.concepro.com/dropbox-public-files/logos/1-logos-abhishek/Signature-Stamp-Abhishek-Paliwal-Name-Logo-Wide-New-Dark.png";
+                } else if (logoImageUrl == "anu1") {
+                  logoImageUrl = "http://downloads.concepro.com/dropbox-public-files/logos/2-logos-anupama/Anupama-Paliwal-787Px-Black-White-Fullname.png";
+                } else if (logoImageUrl == "anu2") {
+                  logoImageUrl = "http://downloads.concepro.com/dropbox-public-files/logos/2-logos-anupama/Anupama-Paliwal-787Px-Colored-Fullname.png";
+                } else if (logoImageUrl == "ap0") {
+                  logoImageUrl = "http://downloads.concepro.com/dropbox-public-files/logos/1-logos-abhishek/Abhishek-Name-Stamp-Gold.png";
+                } else if (logoImageUrl == "concepro1") {
+                  logoImageUrl = "http://downloads.concepro.com/dropbox-public-files/logos/4-logos-concepro/Concepro-Shadow.png";
+                } else if (logoImageUrl == "concepro2") {
+                  logoImageUrl = "http://downloads.concepro.com/dropbox-public-files/logos/4-logos-concepro/I-Love-Concepro-Blue.png";
+                } else if (logoImageUrl == "conceproc1") {
+                  logoImageUrl = "http://downloads.concepro.com/dropbox-public-files/logos/4-logos-concepro/christmas-concepro-logo-with-santa-reindeer.png";
+                } else if (logoImageUrl == "conceproc2") {
+                  logoImageUrl = "http://downloads.concepro.com/dropbox-public-files/logos/4-logos-concepro/christmas-concepro-hat-logo.png";
+                } else {
+                  logoImageUrl = logoImageUrl;
+                }
+
+                logoImageUrl = "<img src='" + logoImageUrl + "' height='90px'>" ;
+
+                      if (logoImageUrl == "") {
+                        logoImageUrl = "" ;
+                      } else {
+                        logoImageUrl = logoImageUrl ;
+                      }
+
+
         quoteText = document.getElementById("quoteText").value.toUpperCase(); ;
 
         /* Calculations begin */
@@ -35,8 +72,8 @@ function randRGBA(min,max)
 
         }
 
-        document.getElementById("demo1").innerHTML = "<p class='dark'>" + headingName + "<br>&mdash;</p>" + "<div class='pali' style=\"background-color:" + colorRGBA + ";\">" + text + "</div>" + "<br><img src='http://downloads.concepro.com/dropbox-public-files/logos/1-logos-abhishek/From-The-Desk-Of-Abhishek-Royal.png' height='100px'>" ;
+        document.getElementById("demo1").innerHTML = "<p class='dark'>" + headingName + "<br>&mdash;</p>" + "<div class='pali' style=\"background-color:" + colorRGBA + ";\">" + text + "</div>" + "<br>" + logoImageUrl ;
 
-        document.getElementById("demo1text").innerHTML = "<p class='dark'>" + headingName + "<br>&mdash;</p>" + "<div class='pali' style=\"color:" + colorRGBA + ";\">" + text + "</div>"+ "<p class='dark'>&mdash; DESIGN BY &mdash;</p><img src='http://downloads.concepro.com/dropbox-public-files/logos/1-logos-abhishek/Abhishek-Paliwal-125-Circle-fullname.png' height='70px'>" ;
+        document.getElementById("demo1text").innerHTML = "<p class='dark'>" + headingName + "<br>&mdash;</p>" + "<div class='pali' style=\"color:" + colorRGBA + ";\">" + text + "</div>"+ "<p class='dark'>&mdash; DESIGN BY &mdash;</p>" + logoImageUrl ;
 
 }

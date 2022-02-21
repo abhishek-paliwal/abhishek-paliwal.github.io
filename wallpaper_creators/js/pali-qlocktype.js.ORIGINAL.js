2abac6cbc29637a92248a5523f0eb0b85f20876a
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-02-21 22:31:00 +0200

    wordle styled wallpaper added

--- wallpaper_creators/js/pali-qlocktype.js.ORIGINAL.js
+++ wallpaper_creators/js/pali-qlocktype.js.ORIGINAL.js
@@ -52,93 +52,28 @@ function randRGBA(min,max)
 
         /* Calculations begin */
         warr = quoteText.split(' ');
-        warr_elements_length_array = warr.map(a => a.length) ;
-        longest_word_length = Math.max(...warr_elements_length_array) ;
 
         var randstring = 'DOLORSITAMETCONSECTETURADIPISICINGELITSEDDOEIUSMODTE';
 
         i=0;
         text ="";
         text1 ="";
-        //
         for (i = 0; i < warr.length; i++) {
+
             remlength = 15 - warr[i].length;
+
             randstart = Math.floor((Math.random() * remlength) );
+
             textbegin = randstring.substring(randstart, remlength);
+
             textend = randstring.substring(remlength, remlength + randstart);
-            //
+
             text += "<br>" + "<span class='palicolor'>" + textbegin + "</span>" + warr[i] +  "<span class='palicolor'>" + textend + "</span>";
-            ///////
-            warr_letter_array = warr[i].split('') ;  
-
-            main_div_block = "<div style='margin: 3px; display: inline-flex ; width: 30px ; height: 30px; color: white; font-family: sans-serif; align-items: center; vertical-align: middle; justify-content: center; line-height: 30px ; font-weight: bold ; background-color: " 
-            
-            wordle_yellow = "#c9b458" ; 
-            wordle_green = "#6aaa64" ;
-            wordle_darkgrey = "#787c7e" ;
-            wordle_lightgrey = "#c0c0c0" ;
-            wordle_white = "#ffffff" ;
-            wordle_border_color = "#d3d6da" ;
-            
-            style_yellow = main_div_block + wordle_yellow + "; '>" ;
-            style_green = main_div_block + wordle_green + "; '>" ;
-            style_darkgrey = main_div_block + wordle_darkgrey + "; '>" ;
-            style_lightgrey = main_div_block + wordle_lightgrey + "; '>" ;
-            style_white = main_div_block + wordle_white + "; '>" ;
-            style_empty_block = main_div_block + wordle_white + "; border: 1px solid " + wordle_border_color + " ; '>" ;
-
-            // longest_word_length = 10 ; 
-            insert_more_letters = longest_word_length - warr_letter_array.length ; 
-            last_word = warr[warr.length -1] ; 
-            last_word_letter_array = last_word.split('') ; 
-            console.log(last_word_letter_array) ;
-            //         
-            //console.log(warr[i]) ;
-            //console.log(warr_letter_array) ;
-            //console.log('LENGTH = ' + warr_letter_array.length) ;
-            //
-            for (j = 0; j < warr_letter_array.length; j++) {
-              /* ======== */
-              // Formatting for last word in array
-              if ( i === warr.length -1 ) {
-                text1 += style_green + warr_letter_array[j] + "</div>" ;
-              //console.log(warr_letter_array[j]) ;
-              } else {
-                /* Check if current alphabet is present in last word of array, then format accordingly */
-                if ( last_word_letter_array.includes(warr_letter_array[j]) ) {
-                    console.log('Present: ' +  typeof(warr_letter_array[j]) + ' = ' + typeof(last_word_letter_array) ) ;
-                    ////// Formatting for exact positional match for alphabet
-                    if (j === last_word_letter_array.indexOf(warr_letter_array[j]) ) {
-                      text1 += style_green + warr_letter_array[j] + "</div>" ;
-                    } else { // Formatting for non-positional match for alphabet
-                      text1 += style_yellow + warr_letter_array[j] + "</div>" ;
-                    } ;
-                    ////// 
-                } else { // Formatting for no match for alphabet
-                  text1 += style_darkgrey + warr_letter_array[j] + "</div>" ;
-                  console.log('Not present: ' +  typeof(warr_letter_array[j]) + ' = ' + typeof(last_word_letter_array) ) ;
-                }
-                /* ////////// */               
-              }             
-            /* ======== */
-            } ;
-            //////////
-            // Formatting for empty blocks (preferably use one of these: style_darkgrey // style_empty_block // style_white)
-            for (k = 0; k < insert_more_letters; k++) {
-              text1 += style_darkgrey + "" + "</div>" ; 
-              //text1 += style_empty_block + "" + "</div>" ; 
-              //text1 += style_white + "" + "</div>" ; 
-            }
-            text1 += "<br>" ; 
-           /////////
 
         }
-
 
         document.getElementById("demo1").innerHTML = "<p class='dark'>" + headingName + "<br>&mdash;</p>" + "<div class='pali' style=\"background-color:" + colorRGBA + ";\">" + text + "</div>" + "<br>" + logoImageUrl ;
 
         document.getElementById("demo1text").innerHTML = "<p class='dark'>" + headingName + "<br>&mdash;</p>" + "<div class='pali' style=\"color:" + colorRGBA + ";\">" + text + "</div>"+ "<p class='dark'>&mdash;</p>" + logoImageUrl ;
-
-        document.getElementById("demowordle1").innerHTML = "<p class='dark'>" + headingName + "<br>&mdash;</p>" + "<div class='paliwordle' style=\"background-color:" + 'white' + ";\">" + text1 + "</div>" + "<br>" + logoImageUrl ;
 
 }
